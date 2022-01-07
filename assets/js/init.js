7f5b053ec8c768cd25b72ab86229424536a8353e
Ivan Otarola <iotarolal@gmail.com>
2022-01-06 21:20:31 -0300

    Validacion de formulario

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,3 +1,5 @@
+import {Leon, Lobo, Oso, Serpiente, Aguila} from './Animales.js'
+
 (async function () {
     let data = await fetch('data/animales.json')
     const data2 = await data.json()
@@ -7,24 +9,68 @@
         // obtengo el valor del "option" seleccionado
         const nombre_animal = $('#animal').val();
 
-        // busco animal seleccionado
         const animal = animales.find( an => an.name == nombre_animal )
 
-        // muestro imagen animal seleccionadoen registro animal 
         $('#preview').css('background-image', `url("assets/imgs/${animal.imagen}")`)
         console.log("entere al iife")
 
 
-        $('#btnRegistrar').on('click', function() {
-            $('#Animales').append(`
-            <div class="col-md-3 p-3">
-                <img class="d-block w-100" src="assets/imgs/${animal.imagen}" alt="imagen">
-            </div>`)
-
-        })
-            
-
     })
 
-    console.log(animales);
 })()  // IIFE (Funciones Inmediatamente Ejecutadas)
+
+
+$('#btnRegistrar').on('click', function() {
+
+    const nombre_animal = $('#animal').val();
+    const rangoanios = $('#edad').val();
+    const comentarios = $('#comentarios').val().trim();
+
+    // Valido datos de entrada y seleccióm
+    const errores = [];
+    if (nombre_animal = "") {
+        errores.push("Debe seleccionar un Animal");
+    } 
+
+    if (rangoanios = "") {
+        errores.push("Debe seleccionar Rango de Edad");
+    } 
+
+    if (comentarios = "") {
+        errores.push("Debe ingresar comentario");
+    } 
+    
+    if (errores.length > 0){
+        alert(errores);
+        return;  
+    }
+
+    // Condiciono por nombre e instancio un objeto
+    let nuevo_animal
+    if (nombre_animal == "Leon") {
+        nuevo_animal = new Leon (nombre_animal, rangoanios, comentarios);
+        } else if (nombre_animal == "Lobo") {
+            nuevo_animal = new Lobo (nombre_animal, rangoanios,  comentarios);
+            } else if (nombre_animal == "Oso") {
+                nuevo_animal = new Oso (nombre_animal, rangoanios,  comentarios);
+                } else if (nombre_animal == "Serpiente") {
+                        nuevo_animal = new Serpiente (nombre_animal, rangoanios, comentarios);
+                    } else if (nombre_animal == "Aguila") {
+                        nuevo_animal = new Aguila (nombre_animal, rangoanios,  comentarios);
+                    }
+
+// Llamo a la Funcion para dibujar el animal a registrar
+    dibujoanimales(nuevo_animal);   
+})
+
+// Funcion para dibujat animal a registrar
+function dibujoanimales(nuevo_animal) {
+    $('#Animales').append(`
+        <div class="card m-1 bg-secondary" style="max-width: 12rem">
+            <img src="${nuevo_animal.img}" class="card-img-top" alt="animal">
+            <div class="card-body py-1”>
+            <button type="button" class="btn btn-outline-light">Light</button>
+            </div>
+        </div>
+    `)
+}
---
+++ assets/js/registra.js
@@ -0,0 +1,32 @@
+(async function () {
+    let data = await fetch('data/animales.json')
+    const data2 = await data.json()
+    const animales = data2.animales
+
+    $('#animal').on('change', function() {
+        // obtengo el valor del "option" seleccionado
+        const nombre_animal = $('#animal').val();
+
+        // busco animal seleccionado
+        const animal = animales.find( an => an.name == nombre_animal )
+
+        // muestro imagen animal seleccionadoen registro animal 
+        $('#preview').css('background-image', `url("assets/imgs/${animal.imagen}")`)
+        console.log("entere al iife")
+
+
+        $('#btnRegistrar').on('click', function() {
+            $('#Animales').append(`
+            <div class="col-md-3 p-3">
+                <img class="d-block w-100" src="assets/imgs/${animal.imagen}" alt="imagen">
+            </div>`)
+
+        })
+            
+
+    })
+
+    console.log(animales);
+})()  // IIFE (Funciones Inmediatamente Ejecutadas)
+
+
